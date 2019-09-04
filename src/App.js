c42dc013e219ff24b3bc5bcb7f5532aea296c328
Osmar CS <osmar.costa.s@gmail.com>
2019-09-03 23:46:07 -0300

    💄 import profile.png

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css'
+import profile from './assets/profile.png';
 
 export default function App() {
-  return (<h1>Hello World</h1>);
+  return (
+    <>
+      <h1>Hello World</h1>
+      <img src={profile} />
+    </>
+  );
 }
